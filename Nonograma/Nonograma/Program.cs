2b8badc28e9e1edcbb28cb103bfabbcd112a0de6
elisatodd <elitodd@ucm.es>
2021-05-24 09:37:29 +0200

    Mejoras de rendimiento, listas

--- Nonograma/Nonograma/Program.cs
+++ Nonograma/Nonograma/Program.cs
@@ -7,44 +7,127 @@ namespace Nonograma
         static bool Debug = false;
         static void Main(string[] args)
         {
-            Tablero tab = new Tablero(0);
 
-            tab.Dibuja();
+            int nivelActual = 0;
+            int niveles = 4; // Número de niveles con los que cuenta el tablero
+            Tablero[] nivelesSuperados = new Tablero[niveles];
+
+            int modoDeJuego = -1;
 
             char c = ' ';
 
-            while (!tab.TableroCompletado()) // Bucle principal de juego
+            while(modoDeJuego != 4) //Mientras no escoja salir del juego
             {
-                c = LeeInput();
-                
-                if (c != ' ')
+                while (modoDeJuego != 1 && modoDeJuego != 2 && modoDeJuego != 3 && modoDeJuego != 4)
                 {
-                    if (Debug)
-                    {
-                        Console.WriteLine(c);
-                        Console.Clear();
-                    }
-
-                    if (c == 'l' || c == 'r' || c == 'u' || c == 'd')
-                    {
-                        tab.Mueve(c);
-                    }
-                    else if (c == 'o')
-                    {
-                        tab.Colorea();
-                    }else if (c == 'x')
-                    {
-                        tab.Tacha();
-                    }else if (c == 'b')
-                    {
-                        tab.Borra();
-                    }
-
-                    tab.Dibuja();
-                    c = ' ';
+                    Console.Clear();
+                    Console.WriteLine("1- Jugar.");
+                    Console.WriteLine("2- Ver un tablero completado.");
+                    Console.WriteLine("3- Crear un nonograma.");
+                    Console.WriteLine("4- Salir.");
+                    Console.WriteLine();
+                    Console.WriteLine("Escriba el número correspondiente a la acción que desee realizar.");
+                    modoDeJuego = int.Parse(Console.ReadLine());
                 }
 
+                // Volverá al menú cuando acabe el nivel o pulse q
+                while (modoDeJuego != 0 && modoDeJuego != 4)
+                {
+                    if (modoDeJuego == 1)
+                    {// Juega todos los niveles hasta que decida salir (q)
+                        while (c != 'q' && nivelActual < niveles)
+                        {
+                            Console.Clear();
+                            Tablero tab = new Tablero(nivelActual);
+                            tab.Dibuja();
+                            while (c != 'q' && !tab.TableroCompletado())
+                            {
+                                c = ' ';
+                                c = LeeInput();
+
+                                if (c != ' ')
+                                {
+                                    if (Debug)
+                                    {
+                                        Console.WriteLine(c);
+                                        Console.Clear();
+                                    }
+
+                                    if (c == 'l' || c == 'r' || c == 'u' || c == 'd')
+                                    {
+                                        tab.Mueve(c);
+                                    }
+                                    else if (c == 'o')
+                                    {
+                                        tab.Colorea();
+                                    }
+                                    else if (c == 'x')
+                                    {
+                                        tab.Tacha();
+                                    }
+                                    else if (c == 'b')
+                                    {
+                                        tab.Borra();
+                                    }
+
+                                    tab.Dibuja();
+                                }
+                            } // Bucle de un nivel
+
+                            if (tab.TableroCompletado())
+                            {
+                                nivelesSuperados[nivelActual] = tab;
+                            }
+
+                            if (c != 'q')
+                            {
+                                nivelActual++;
+
+                                if (nivelActual == niveles)
+                                {
+                                    Console.WriteLine("Juego completado. Pulse ENTER para volver al menú.");
+
+                                    Console.ReadLine();
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Tablero completado. Pulse ENTER para pasar al siguiente.");
+
+                                    Console.ReadLine();
+                                }
+
+                            }
+
+
+                        }
+                        modoDeJuego = 0;
+                    }
+                    else if (modoDeJuego == 2)
+                    {
+                        Console.WriteLine("Qué nivel desea ver: ");
+                        int nivelDeseado = int.Parse(Console.ReadLine());
+
+                        if (nivelDeseado >= nivelActual)
+                        {
+                            Console.WriteLine("Aún no puede ver el nivel " + nivelDeseado + " , no lo ha superado.");
+                            Console.WriteLine("Pulse ENTER para volver al menú.");
+                            Console.ReadLine();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Tablero tab = nivelesSuperados[nivelDeseado];
+                            tab.DibujaSolucion();
+
+                            Console.WriteLine("Pulse ENTER para volver al menú.");
+                            Console.ReadLine();
+                        }
+                        modoDeJuego = 0;
+                    }
+
+                }
             }
+           
         }
 
 
@@ -82,6 +165,9 @@ namespace Nonograma
                     break;
                 case "H": // Cargar
                     dir = 'h';
+                    break;
+                case "Q": // salir
+                    dir = 'q';
                     break;
 
             }
--- Nonograma/Nonograma/Tablero.cs
+++ Nonograma/Nonograma/Tablero.cs
@@ -14,11 +14,21 @@ namespace Nonograma
         string[] datosFilas, datosColumnas; // Contienen los datos sobre qué debe colocarse en cada casilla
         int maxDatosFil, maxDatosCol; // Indican el espacio que se debe dejar entre el borde de la consola y el tablero
         Coor jugador; // Posición del jugador
-        // Contiene los datos necesarios para que la condición del juego se complete
-        Lista listaDatos = new Lista();
+
+        // Sirve para que, con tableros grandes, la ejecución sea más fluida
+        // Sólo llevará a cabo la comprobación cuando haya las suficientes casillas rellenas: si hay más o menos, es absurdo comprobar
+        int casillasColoreadas = 0, casillasNecesarias = 0;
+
+
+        // Los niveles ya superados
+        Tablero[] nivelesSuperados = new Tablero[4];
+
+        Lista[] vectorDatosColumnas;
+        Lista[] vectorDatosFilas;
 
         public Tablero(int n) // Crea un tablero del nivel indicado 
         {
+                    /// LECTURA DE TABLERO
             StreamReader levels = new StreamReader("levels.txt");
 
             string s = levels.ReadLine();
@@ -61,6 +71,7 @@ namespace Nonograma
 
             levels.Close();
 
+                    /// CREACIÓN Y RELLENO DE TABLERO
             tab = new Casillas[filas, columnas];
 
             // Todas las casillas están libres al principio
@@ -72,19 +83,50 @@ namespace Nonograma
                 }
             }
 
-            // Se rellena la lista de datos: Sólo hace falta comprobar una: filas o columnas, si una está correcta, la otra también lo estará
+                    /// CREACIÓN Y RELLENO DEL VECTOR DE LISTAS
+            // El vector tomará tantos argumentos como columnas tenga el tablero, porque comprueba los datos de las columnas
+            vectorDatosColumnas = new Lista[columnas];
+            // Se rellena el array de listas de datos
             for (int i = 0; i < datosColumnas.Length; i++)
             {
                 for (int j = 0; j < datosColumnas[i].Length; j++)
                 {
                     if (datosColumnas[i][j] != ',')
                     {
-                        listaDatos.InsertaFinal(int.Parse(datosColumnas[i][j].ToString()));
+                        // Si es el primer nodo en esa posición, se debe crear uno nuevo
+                        if (vectorDatosColumnas[i] == null)
+                        {
+                            vectorDatosColumnas[i] = new Lista();
+                        }
+
+                        vectorDatosColumnas[i].InsertaFinal(int.Parse(datosColumnas[i][j].ToString()));
+                        casillasNecesarias+= int.Parse(datosColumnas[i][j].ToString());
                     }
                 }
-                listaDatos.InsertaFinal(0); // El 0 indica cambio de columna
             }
 
+            // El vector tomará tantos argumentos como filas tenga el tablero, porque comprueba los datos de las filas
+            vectorDatosFilas = new Lista[filas];
+            // Se rellena el array de listas de datos
+            for (int i = 0; i < datosFilas.Length; i++)
+            {
+                for (int j = 0; j < datosFilas[i].Length; j++)
+                {
+                    if (datosFilas[i][j] != ',')
+                    {
+                        // Si es el primer nodo en esa posición, se debe crear uno nuevo
+                        if (vectorDatosFilas[i] == null)
+                        {
+                            vectorDatosFilas[i] = new Lista();
+                        }
+
+                        vectorDatosFilas[i].InsertaFinal(int.Parse(datosFilas[i][j].ToString()));
+                    }
+                }
+            }
+
+
+            /// JUGADOR
             // Se le asigna al jugador una posición inicial
             jugador = new Coor(0, 0);
 
@@ -214,6 +256,7 @@ namespace Nonograma
         }
 
 
+                /// MOVIMIENTO
         // Método que devuelve si la siguiente posición en la dirección dir está dentro del tablero
         private bool SiguientePosicion(Coor dir, out Coor nextPos)
         {
@@ -258,90 +301,312 @@ namespace Nonograma
         }
 
 
+                /// CAMBIO DE CASILLAS
         // Pone el valor de la casilla actual como coloreada (en negro)
         public void Colorea()
         {
             tab[jugador.fil, jugador.col] = Casillas.Coloreada;
+            casillasColoreadas++;
         }
 
         // Pone el valor de la casilla actual como tachada (con una cruz)
         public void Tacha()
         {
+            if (tab[jugador.fil, jugador.col] == Casillas.Coloreada)
+            {
+                casillasColoreadas--;
+            }
             tab[jugador.fil, jugador.col] = Casillas.Tachada;
         }
 
         // Pone el valor de la casilla actual como libre (sin nada)
         public void Borra()
         {
+            if (tab[jugador.fil, jugador.col] == Casillas.Coloreada)
+            {
+                casillasColoreadas--;
+            }
             tab[jugador.fil, jugador.col] = Casillas.Libre;
         }
 
 
+                /// BUCLE DE JUEGO
         // Determina si el tablero está completo o no:
         // Está completo si se cumplen todas las condiciones
         public bool TableroCompletado()
         {
             bool completado = true;
 
-            Lista tableroActual = new Lista();
-
-            // Lleva la cuenta de la última posición que se ha escrito en la lista
-            // sirve para pasar a la siguiente posición cuando se cambia de columna
-            // y cuando hay separaciones dentro de una misma columna
-            int cont = 0;
-
-            // Se rellena la lista de datos actuales con la información del tablero
-            for (int i = 0; i < tab.GetLength(1); i++)
+            if (casillasColoreadas != casillasNecesarias)
             {
-                for (int j = 0; j < tab.GetLength(0); j++)
+                completado = false;
+            }
+            else
+            {
+                // Primero comprueba las columnas
+                Lista[] vectorActualColumnas = new Lista[tab.GetLength(1)];
+
+                int contador = 0;
+                // relleno el vector de la versión actual del tablero 
+                for (int i = 0; i < tab.GetLength(1); i++)
                 {
-                    if (tab[j,i] == Casillas.Coloreada)
+                    for (int j = 0; j < tab.GetLength(0); j++)
                     {
-                        try{ // si es la n-esima casilla de un grupo de casillas seguidas
-                            tableroActual.InsertaNesimo(cont, tableroActual.nEsimo(cont) + 1);
-                            tableroActual.BorraUltimoNodo();
-                        }
-                        catch (Exception k)
-                        { // es la primera casilla de un grupo de casillas seguidas
-                            tableroActual.InsertaFinal(1);
+                        if (vectorActualColumnas[i] == null)
+                        {
+                            vectorActualColumnas[i] = new Lista();
                         }
 
-                        if (j == tab.GetLength(0) - 1)// última posición de la columna
+                        if (tab[j, i] == Casillas.Coloreada)
                         {
-                            cont++;
+                            try
+                            { // si es la n-esima casilla de un grupo de casillas seguidas
+                                vectorActualColumnas[i].InsertaNesimo(contador, vectorActualColumnas[i].nEsimo(contador) + 1);
+                                vectorActualColumnas[i].BorraUltimoNodo();
+                            }
+                            catch (Exception k)
+                            { // es la primera casilla de un grupo de casillas seguidas
+                                vectorActualColumnas[i].InsertaFinal(1);
+                            }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                int k = vectorActualColumnas[i].nEsimo(contador);
+                                contador++;
+                            }
+                            catch (Exception k)
+                            {
+
+                            }
                         }
                     }
-                    else // es un espacio en blanco o tachado
+                    contador = 0;
+                }
+
+                int m = 0;
+                // recorro cada posición del array = cada columna
+                while (m < vectorDatosColumnas.Length && completado)
+                {
+                    int n = 0;
+
+                    while (n < vectorDatosColumnas[m].CuentaEltos() && completado)
                     {
                         try
                         {
-                            if (tableroActual.nEsimo(cont) >= 0)
+                            int nEsimoActual = vectorActualColumnas[m].nEsimo(n);
+                            if (nEsimoActual != vectorDatosColumnas[m].nEsimo(n))
                             {
-                                cont++;
+                                completado = false;
                             }
                         }
-                        catch (Exception k)
+                        catch (Exception k) // el correcto tiene un valor en n pero el actual no
                         {
+                            completado = false;
                         }
+
+                        n++;
+                    }
+                    m++;
+                }
+                 // Si las columnas son correctas, comprueba las filas
+
+                if (completado)
+                {
+                    Lista[] vectorActualFilas = new Lista[tab.GetLength(0)];
+
+                    // relleno el vector de la versión actual del tablero 
+                    for (int i = 0; i < tab.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < tab.GetLength(1); j++)
+                        {
+                            if (vectorActualFilas[i] == null)
+                            {
+                                vectorActualFilas[i] = new Lista();
+                            }
+
+                            if (tab[i,j] == Casillas.Coloreada)
+                            {
+                                try
+                                { // si es la n-esima casilla de un grupo de casillas seguidas
+                                    vectorActualFilas[i].InsertaNesimo(contador, vectorActualFilas[i].nEsimo(contador) + 1);
+                                    vectorActualFilas[i].BorraUltimoNodo();
+                                }
+                                catch (Exception k)
+                                { // es la primera casilla de un grupo de casillas seguidas
+                                    vectorActualFilas[i].InsertaFinal(1);
+                                }
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    int k = vectorActualFilas[i].nEsimo(contador);
+                                    contador++;
+                                }
+                                catch (Exception k)
+                                {
+
+                                }
+                            }
+                        }
+                        contador = 0;
+                    }
+
+                    m = 0;
+                    // recorro cada posición del array = cada fila
+                    while (m < vectorDatosFilas.Length && completado)
+                    {
+                        int n = 0;
+
+                        while (n < vectorDatosFilas[m].CuentaEltos() && completado)
+                        {
+                            try
+                            {
+                                int nEsimoActual = vectorActualFilas[m].nEsimo(n);
+                                if (nEsimoActual != vectorDatosFilas[m].nEsimo(n))
+                                {
+                                    completado = false;
+                                }
+                            }
+                            catch (Exception k) // el correcto tiene un valor en n pero el actual no
+                            {
+                                completado = false;
+                            }
+
+                            n++;
+                        }
+                        m++;
                     }
                 }
-                tableroActual.InsertaFinal(0); // El 0 indica cambio de columna
-                cont++;
             }
-
-            int n = 0;
-            // Compara el estado actual con el de listaDatos (el correcto)
-            while (n < listaDatos.CuentaEltos() && completado)
-            {
-                // Compara cada elemento de la lista
-                if (tableroActual.nEsimo(n) != listaDatos.nEsimo(n))
-                {
-                    completado = false;
-                }
-                n++;
-            }
-
             return completado;
         }
+
+        void GuardaTableroSuperado(int i, Tablero tab)
+        {
+            // Se referencia este mismo tablero
+
+            /* StreamWriter nivelesCopia = new StreamWriter("nivelesSuperadosCopia");
+            StreamReader niveles = new StreamReader("nivelesSuperados");
+
+            string s = niveles.ReadLine();
+            while(s != " " && s != null)
+            {
+                nivelesCopia.WriteLine(s);
+                s = niveles.ReadLine();
+            }
+
+            nivelesCopia.WriteLine("level" + i);
+            nivelesCopia.WriteLine(); */
+
+        }
+
+        // Dibuja sin tener cuenta al jugador
+        public void DibujaSolucion()
+        {
+            int cont = 0; // variable que ayudará con el dibujo 
+
+            // En primer lugar escribo los datos
+            // Datos de las columnas
+            Console.SetCursorPosition(maxDatosFil * 2 + 2, 0); // Lo que ocupan los datos de la izquierda, más el centrado
+            for (int i = 0; i < datosColumnas.Length; i++)
+            {
+                for (int j = 0; j < datosColumnas[i].Length; j++)
+                {
+                    if (datosColumnas[i][j] != ',')
+                    {
+                        cont++;
+                        Console.Write(datosColumnas[i][j]);
+                        // Si el dato tiene dos cifras
+                        if ((j + 1) < datosColumnas[i].Length && datosColumnas[i][j + 1] != ',')
+                        {
+                            Console.Write(datosColumnas[i][j + 1]);
+                            j++;
+                        }
+                    }
+                    else
+                    {
+                        Console.SetCursorPosition(maxDatosFil * 2 + 4 * (i) + 2, cont);
+                    }
+                }
+                cont = 0;
+                Console.SetCursorPosition(maxDatosFil * 2 + 4 * (i + 1) + 2, 0);
+            }/// ESTO ESTÁ BIEN PARA EL ANCHO 4, SI ES ANCHO DOS, SE CAMBIA EL 4 POR UN 2 Y SE QUITA EL + 2
+
+
+            // Datos de las filas
+            Console.SetCursorPosition(0, maxDatosCol);
+            for (int i = 0; i < datosFilas.Length; i++)
+            {
+                for (int j = 0; j < datosFilas[i].Length; j++)
+                {
+                    if (datosFilas[i][j] != ',')
+                    {
+                        Console.Write(datosFilas[i][j]);
+                        // Si el dato tiene dos cifras
+                        if ((j + 1) < datosFilas[i].Length && datosFilas[i][j + 1] != ',')
+                        {
+                            Console.Write(datosFilas[i][j + 1]);
+                            j++;
+                        }
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.SetCursorPosition(0, maxDatosCol + (i + 1) * 2); /// SI SE HACE DE TAMAÑO MENOR, QUITAR *2
+            }
+
+            // Hay que cambiar la posición del puntero: 
+            Console.SetCursorPosition(maxDatosFil * 2, maxDatosCol); // Dos huecos para el número, y uno entre medias
+
+            // Ahora dibujo el tablero en sí
+            cont = 1;
+            // Además, si estoy en esa fila y columna concretas, se dibujan diferente
+            for (int n = 0; n < tab.GetLength(0); n++)
+            {
+                for (int j = 1; j < 3; j++)
+                {
+                    for (int m = 0; m < tab.GetLength(1); m++)
+                    {
+                        if (tab[n, m] == Casillas.Coloreada)
+                        {
+                            Console.BackgroundColor = ConsoleColor.DarkGray;
+                            
+                            Console.Write("    ");
+                        }
+                        else if (tab[n, m] == Casillas.Tachada)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+                            if (j == 1) // En la "primera vuelta"
+                            {
+                                Console.Write("  / ");
+                            }
+                            else
+                            {
+                                Console.Write(" /  ");
+                            }
+                        }
+                        else if (tab[n, m] == Casillas.Libre)
+                        {
+                            Console.BackgroundColor = ConsoleColor.White;
+                            
+                            Console.Write("    ");
+                        }
+                    }
+                    Console.SetCursorPosition(maxDatosFil * 2, maxDatosCol + cont);
+                    cont++;
+                }
+            }
+
+            // Reseteo de los colores
+            Console.BackgroundColor = default;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
     }
 }
